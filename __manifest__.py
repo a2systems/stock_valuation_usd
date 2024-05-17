d6a74d7c76d0dd64535d74c6074630afa8d17e10
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2024-05-17 18:43:15 +0000

    Se agrego USD a la vista de margenes en USD

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Stock Valuation USD",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "category": "Stock",
     "installable": True,
     "application": False,
     "summary": "Stock Valuation USD",
-    "depends": ["stock_account"],
+    "depends": ["stock_account","product_margin"],
     "data": [
         "views/stock.xml", 
         "views/stock_quant.xml", 
--- models/product.py
+++ models/product.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models, _, api
-
+from datetime import date
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
@@ -13,4 +13,16 @@ class ProductProduct(models.Model):
             value_usd = sum(valuation_layers.mapped('value_usd'))
             rec.total_value_usd = value_usd
 
+    def _compute_total_margin_usd(self):
+        for rec in self:
+            res = 0
+            res = self.env.user.company_id.currency_id._convert(
+                rec.total_margin,
+                self.env.ref('base.USD'),
+                self.env.user.company_id,
+                date.today(),
+                )
+            rec.total_margin_usd = res
+
     total_value_usd = fields.Float('Valor Total USD',compute=_compute_total_value_usd)
+    total_margin_usd = fields.Float('Margen USD',compute=_compute_total_margin_usd)
